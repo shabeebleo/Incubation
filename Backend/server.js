0656d1b6a133092c711350350972945aea0321e9
shabeebleo <shabeebpv0786@gmail.com>
2022-10-20 14:44:23 +0530

    mongo db connection

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,11 @@ const dotenv = require ('dotenv').config()
 const {errorHandler}=require('./middleware/errorMiddleware')
 const port =process.env.PORT || 5000
 const  app=express()
-
-app.use(express.json())
+const colors =require('colors')
+const  connectDB=require('./config/db')
+ 
+connectDB()
+app.use(express.json()) 
 app.use(express.urlencoded({extended:false}))
 app.use('/user', require('./routes/goalRoutes'))
 app.use('/admin', require('./routes/goalRoutes'))
