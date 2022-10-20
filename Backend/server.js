83810808fc39a8b68f1ae780fbf0c26b27c8d5a7
shabeebleo <shabeebpv0786@gmail.com>
2022-10-20 13:53:01 +0530

    routes changed to user

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const  app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/user', require('./routes/goalRoutes'))
+app.use('/admin', require('./routes/goalRoutes'))
 app.use(errorHandler)
 app.listen(port,()=>console.log(`server started at port ${port}`))
